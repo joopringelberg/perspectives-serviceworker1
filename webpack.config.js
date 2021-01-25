0cbacd4bd3448a06fcee10c59793ca65d806198e
Joop Ringelberg <joop.ringelberg@gmail.com>
2021-01-25 15:51:21 +0100

    First commit of service worker package

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    serviceWorker: "./src/service.js"
+  },
+  output: {
+    library: 'perspectives-[name]',
+    filename: 'perspectives-[name].js',
+    path: path.join(__dirname, "dist"),
+    libraryTarget: "umd"
+  },
+  watch: false,
+  mode: "development",
+  target: "webworker",
+  module: {
+    rules: [{
+        test: /.js?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                '@babel/preset-env'
+              ]
+            }
+          }
+        ]
+      }]
+  },
+  externals: {
+    // These are Affjax dependencies when running on node.
+    "xhr2-cookies": {
+      commonjs: "xhr2-cookies",
+      commonjs2: "xhr2-cookies",
+      amd: "xhr2-cookies",
+      root: "xhr2-cookies"
+    },
+    "url": {
+      commonjs: "url",
+      commonjs2: "url",
+      amd: "url",
+      root: "url"
+    }
+  }
+};
